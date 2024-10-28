cd0d3b65df52ecdf2d8b85cea5cbd30a5f70c09f
skyler21 <jeff.miller@rogers.com>
2024-10-28 13:18:33 -0400

    add getTeams and fix Helm

--- src/app.py
+++ src/app.py
@@ -1,5 +1,17 @@
 from flask import Flask,render_template
 import socket
+import sys
+import os
+import logging
+
+
+logging.basicConfig(stream=sys.stdout, 
+    format='%(asctime)s|%(levelname)s|%(thread)s|%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG)
+
+self.urlPort = config.HOCKEY2024APP_HOCKEY_SERVICE_PORT
+logging.info(f"Service URL and Port: {self.urlPort}")
 
 app = Flask(__name__)
 
@@ -13,7 +25,25 @@ def index():
         return render_template('error.html')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+@app.route("/getTeams")
+def getTeams():
+  
+  url = f"http://{self.urlPort}/db/get/teams"
+
+  resp = http.request('GET', url)
+  print(resp.status)
+  print(resp.json())
+
+  json_html = resp.json()
+
+  print(json_html)
+
+  # returning index.html and list 
+  # and length of list to html page 
+  return render_template("teams.html", len = len(json_html), json_html = json_html) 
+   
+  app.run(use_reloader = True, debug = True) 
 
     
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
